283b9fa47b6bf473be6eef1505bf584e93b6cd62
NHau2803 <hau2803nch@gmail.com>
2021-06-26 16:59:38 +0700

    up-code

--- speak.py
+++ speak.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import os
 
-output = gTTS(text = "hello! ", lang = "en", slow=False)
+output = gTTS(text = "ok", lang = "en", slow=False)
 output.save("speak.mp3")
 os.system("speak.mp3")
 
